606c307db14d63c01ebdc4bf28a1d3b889fc2dd5
faith110 <chofaith05@gmail.com>
2026-05-30 01:45:11 +0100

    corrections

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,12 +1,14 @@
 const url = "data/members.json";
 const container = document.querySelector("#members");
 
+// FETCH DATA
 async function getMembers() {
     const response = await fetch(url);
     const data = await response.json();
     displayMembers(data);
 }
 
+// DISPLAY MEMBERS
 function displayMembers(members) {
     container.innerHTML = "";
 
@@ -38,6 +40,15 @@ document.getElementById("gridBtn").addEventListener("click", () => {
 document.getElementById("listBtn").addEventListener("click", () => {
     container.classList.add("list");
     container.classList.remove("grid");
+});
+
+
+// HAMBURGER MENU
+const menuBtn = document.getElementById("menu");
+const nav = document.querySelector("nav ul");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("open");
 });
 
 
